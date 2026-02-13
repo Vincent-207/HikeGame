9e2f88a7a0cf97eb6ac34c2d4b926f5537c65432
Vincent-207 <vhannah2007@outlook.com>
2026-02-13 13:23:16 -0600

    Lock slot

---
+++ Assets/_Scripts/LockSlot.cs
@@ -0,0 +1,35 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LockSlot : MonoBehaviour
+{
+    [SerializeField]
+    int currentValue = 0;
+    [SerializeField] TMP_Text valueText;
+    public void Increment()
+    {
+        currentValue++;
+        if(currentValue > 9)
+        {
+            currentValue = 0;
+        }
+        DisplayValue();
+    }
+
+    public void Decrement()
+    {
+        currentValue --;
+        if(currentValue < 0)
+        {
+            currentValue = 9;
+        }
+        DisplayValue();
+    }
+
+    void DisplayValue()
+    {
+        valueText.text = String.Format("{0}", currentValue);
+    }
+}
